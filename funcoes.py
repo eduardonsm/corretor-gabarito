4e55bb0fde04e3e16002029724cd16d0af97b384
eduardonsm <eduardosume082@gmail.com>
2023-06-28 00:13:49 -0300

    arquivos comentados

--- funcoes.py
+++ funcoes.py
@@ -7,57 +7,80 @@ def gerar_pdf(respostas):
 
     c = canvas.Canvas("relatorio.pdf")
     x=100
+    # coordenada x
     y= (len(respostas)*130)-60
+    # coordenada y, quantidade de analises * 130 que vai ser a altura da pagina e -60 para ser um espaçamento
     espacamento=20
+    # espacamento entre as frases
 
-    # Definir o tamanho da página
     c.setPageSize((400, len(respostas)*130))
+    # Definir o tamanho da página (largura e altura)
 
     c.setFont("Helvetica", 20)
     c.drawString(x, y+10, "Rendimento dos alunos")
+    # titulo do pdf com a fonte Helvetica tamanho 20
 
     c.setFont("Helvetica", 12)
-
+    # setando o fonte para tamanho 12 pro resto do arquivo
 
     for i,d in enumerate(respostas):
+    # i de indice para enumerar aluno 1, aluno 2 ...
+    # d corresponde a cada dado, que é um lista com as respostas marcadas, o numero de acertos e a sua pontuação
+       
         c.drawString(x, y-espacamento, f"O aluno {i+1} teve os seguintes resultados:")
         c.drawString(x, y-espacamento*2, f'Respostas: {d[0]}')
         c.drawString(x, y-espacamento*3, f'Acertos: {d[1]}')
         c.drawString(x, y-espacamento*4, f'Pontuação: {d[2]}')
+    # coordenada x fixa e a coordenada y - o espacamento para ir descendo na pagina
 
-        y-=espacamento*5
-    # Salvar o PDF
+        y -= espacamento*5
+    # atualizando o valor de y para o proximo dado
+
     c.showPage()
     c.save()
+    # fecha a pagina e salva o PDF
 
 def upar_imagem():
-    filename = filedialog.askopenfilename()
-    return filename
+    formatos = (("Imagens", "*.png;*.jpg;*.jpeg"),)
+    # aqui é definido os formatos de arquivos que podem ser upados, no caso as extensoes de imagens
+    arquivo = filedialog.askopenfilename(filetypes=formatos)
+    return arquivo
+    # é aberto o explorer para o usuario selecionar o arquivo e a funcao o retorna 
 
 def extrairMaiorCtn(img):
+    # funcao responsavel por fazer um recorte do maior contorno da imagem, no caso o gabarito
 
     imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # a imagem é transformada em escala de cinza
+
     imgTh = cv2.adaptiveThreshold(imgGray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 12)
-    # transforma em uma imagem binaria
+    # transforma em uma imagem binaria invertida
 
     contours,hi = cv2.findContours(imgTh,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
-    # procura contorno na imagem (gabarito)
+    # procura contornos na imagem
 
     maiorCtn = max(contours, key = cv2.contourArea)
-    # acha o contorno com maior area
+    # acha o maior contorno com base na area dele
 
     x,y,w,h = cv2.boundingRect(maiorCtn)
     # cria um retangulo no contorno e retorna as coordenadas dele
 
     recorte = img[y:y+h,x:x+w]
     recorte = cv2.resize(recorte,(400,500)) 
-    # recorta a imagem original pra ficar so o gabarito e redimensiona no tamanho correto
+    # recorta a imagem original pra ficar so o retangulo (gabarito) e redimensiona no tamanho correto
 
     return recorte
 
 def selecionar(img):
-        img = cv2.resize(img, (600, 700))
-        x, y, w, h = cv2.selectROI(img)
-        recorte = img[y:y+h, x:x+w]
-        recorte = cv2.resize(recorte, (400, 500))
-        return recorte
+    img = cv2.resize(img, (600, 700))
+    # redimensiona a imagem para facilitar a selecao
+
+    x, y, w, h = cv2.selectROI(img)
+    # essa funcao abre a imagem para o usuário desenhar um retangulo na regiao de interesse (ROI)
+    # e retorna as coordenadas e as dimensoes do retangulo feito pelo usuario
+
+    recorte = img[y:y+h, x:x+w]
+    recorte = cv2.resize(recorte, (400, 500))
+    # recorta a imagem original pra ficar so o retangulo (gabarito) e redimensiona no tamanho correto
+
+    return recorte
--- main.py
+++ main.py
@@ -1,39 +1,51 @@
 import cv2
 from funcoes import gerar_pdf, upar_imagem, extrairMaiorCtn, selecionar
-from tkinter import Tk, Button, filedialog, messagebox, Label
+from tkinter import Tk, Button, messagebox, Label
 
 campos = [(12, 2, 92, 85), (105, 2, 92, 85), (196, 2, 92, 85), (289, 2, 92, 85),
           (12, 95, 92, 85), (105, 95, 92, 85), (196, 95, 92, 85), (289, 95, 92, 85),
           (12, 187, 92, 85), (105, 187, 92, 85), (196,187, 92, 85), (289, 187, 92, 85),
           (12, 285, 92, 85), (105, 285, 92, 85), (196, 285, 92, 85), (289, 285, 92, 85),
           (12, 382, 92, 85), (105, 382, 92, 85), (196, 382, 92, 85), (289, 382, 92, 85)]
+# coordenadas que correspondem a cada alternativa
 
 resp = ['1-A', '1-B', '1-C', '1-D',
         '2-A', '2-B', '2-C', '2-D',
         '3-A', '3-B', '3-C', '3-D',
         '4-A', '4-B', '4-C', '4-D',
         '5-A', '5-B', '5-C', '5-D']
+# todas as alternativas em ordem
 
 respostasCorretas = ["1-A", "2-C", "3-B", "4-D", "5-A"]
 
 respostas_armazenadas = []
+# lista usada para armazenar os resultados das analises
 
 
 def analisar_gabarito(imgTh, respostas=[]):
     for id, vg in enumerate(campos):
         x = int(vg[0])
+        # coordenada x
         y = int(vg[1])
+        # coordenada y
         w = int(vg[2])
+        # largura width
         h = int(vg[3])
+        # altura height
+
+        # ou seja, vai atribuindo x,y,w,h para cada alternativa 
 
         campo = imgTh[y:y + h, x:x + w]
+        # recorta a alternativa na imagem
         area = w * h
         pixel_branco = cv2.countNonZero(campo)
-        percentual = round((pixel_branco / area) * 100, 2)
+        # analisa quantos pixels brancos existem no campo
+
+        percentual = float((pixel_branco / area) * 100)
         if percentual >= 10:
             respostas.append(resp[id])
+        # se for mais de 10% em relacao a area do campo é adicionado nas respostas marcada pelo usuario
 
-    erros = 0
     acertos = 0
     if len(respostas) == len(respostasCorretas):
         for num, res in enumerate(respostas):
@@ -42,12 +54,12 @@ def analisar_gabarito(imgTh, respostas=[]):
                 acertos += 1
             else:
                 print(f'{res} ERRADO, correto: {respostasCorretas[num]}')
-                erros += 1
 
         pontuacao = int(acertos * 2)
+        # considerando que sao 5 questoes, cada questao vale 2 para resultar no maximo em 10
 
         cv2.waitKey(0)
-
+        # essa funcao serve para esperar alguma tecla ser digitada
         print(f'''
         respostas corretas = {respostasCorretas}
         respostas = {respostas}
@@ -58,74 +70,96 @@ def analisar_gabarito(imgTh, respostas=[]):
         dados = [respostas, acertos, pontuacao]
         respostas_armazenadas.append(dados)
         cv2.destroyAllWindows()
+        # fecha as janelas criadas, por exemplo na opcao de selecionar manualmente
     else:
         messagebox.showerror(
             "Erro", "Imagem não reconhecida! Tente selecionar manualmente ou fazer o upload de uma nova imagem.")
-
+        # mensagem de erro caso as respostas lidas nao forem da mesma quantidade das respostas corretas
 
 def upload_e_processar():
     filename = upar_imagem()
     if filename:
         processar_imagem(filename)
+    # upa imagem e chama a funcao para processar a imagem
 
 
 def processar_imagem(imagem):
     imagem_bruto = cv2.imread(imagem)
     imagem = cv2.resize(imagem_bruto, (600, 700))
+    # le a imagem e redimensiona
 
     gabarito = extrairMaiorCtn(imagem)
+    # é recortado o gabarito
+
     imgGray = cv2.cvtColor(gabarito, cv2.COLOR_BGR2GRAY)
+    # a imagem é transformada em escala de cinza
+
     ret, imgTh = cv2.threshold(imgGray, 100, 255, cv2.THRESH_BINARY_INV)
+    # transforma em uma imagem binaria invertida
 
     respostas = []
     analisar_gabarito(imgTh, respostas)
+    # cria um lista para as respostas e chama a funcao para analisar o gabarito passando a imagem binaria
 
 
 def selecionar_manualmente():
     imagem = upar_imagem()
     imagem_bruto = cv2.imread(imagem)
+    # upa a imagem e le
 
     gabarito = selecionar(imagem_bruto)
+    # é extraido o gabarito pela funcao selecionar
 
     imgGray = cv2.cvtColor(gabarito, cv2.COLOR_BGR2GRAY)
+     # a imagem é transformada em escala de cinza
+
     ret, imgTh = cv2.threshold(imgGray, 100, 255, cv2.THRESH_BINARY_INV)
+    # transforma em uma imagem binaria invertida
 
     respostas = []
     analisar_gabarito(imgTh, respostas)
+    # cria um lista para as respostas e chama a funcao para analisar o gabarito passando a imagem binaria
 
 
 root = Tk()
+# cria uma janela
+
 root.title("Correção por imagem")
 root.configure(background='#5865f2', padx=40, pady=40)
+# adiciona o titulo e algumas configuracoes de estilo
 
 largura_janela = 400
 altura_janela = 370
+# define a largura e altura da janela
 
 largura_tela = root.winfo_screenwidth()
 altura_tela = root.winfo_screenheight()
+# define a largura e altura da tela do usuario
 
 posicao_x = (largura_tela - largura_janela) // 2
 posicao_y = (altura_tela - altura_janela) // 2
+# calcula onde é o centro
 
 root.geometry(f"{largura_janela}x{altura_janela}+{posicao_x}+{posicao_y}")
+# essa funcao é usada para definir o tamanho e a posicao da janela, nesse caso esta posicionada no centro
 
-titulo = Label(root, text="Corretor de gabarito", font=(
-    'Helvica', 20, 'bold'), fg='white', background='#5865f2')
+titulo = Label(root, text="Corretor de gabarito", font=('Helvica', 20, 'bold'), fg='white', background='#5865f2')
 titulo.place(relx=0.5, rely=0.12, anchor="center")
+# cria um texto e posiciona centralizado horizontalmente e acima do centro vertical
 
-
-button = Button(root, text="Fazer upload da Imagem", command=upload_e_processar,
-                pady=12, padx=12, bd=0.5, font=('Arial', 12), activebackground='#d1d5ff')
-
+button = Button(root, text="Fazer upload da Imagem", command=upload_e_processar,pady=12, padx=12, bd=0.5, font=('Arial', 12), activebackground='#d1d5ff')
 button.place(relx=0.5, rely=0.45, anchor="center")
+# cria um botao para fazer upload da imagem, chamando a funcao upload_e_processar 
 
-button_selecionar_manualmente = Button(root, text="Selecionar Manualmente", command=selecionar_manualmente,
-                                       padx=12, pady=12, bd=0.5, font=('Arial', 12), activebackground='#d1d5ff')
 
+button_selecionar_manualmente = Button(root, text="Selecionar Manualmente", command=selecionar_manualmente,padx=12, pady=12, bd=0.5, font=('Arial', 12), activebackground='#d1d5ff')
 button_selecionar_manualmente.place(relx=0.5, rely=0.8, anchor="center")
+# cria um botao para fazer a selecao manual da imagem, chamando a funcao selecionar_manualmente
 
 
 root.mainloop()
+# isso faz a janela ficar em loop ate ser fechado
 
 gerar_pdf(respostas_armazenadas)
 print("PDF do rendimento gerado com sucesso.")
+# gera um pdf com as respostas_armazenadas e printa uma mensagem na tela
